5a31212d787e238bf61633ea8a9fe2474683c7ff
ajith-code <70834458+ajith-code@users.noreply.github.com>
2021-05-11 20:30:15 +0530

    Add files via upload
    In an arra identif repeated number

---
+++ duplicatevalue.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+
+int a[]={10,5,3,4,5,6,7};
+
+for(int i=0;i<=6;i++)
+
+{
+
+for(int j=i+1;j<=6;j++)
+
+{
+
+if(a[i]==a[j])
+
+{
+    
+cout<<"DUPLICATE value is"<<a[i]<<"\n";
+
+}
+
+}
+
+}
+           
+
+return 0;
+
+}
